ff486f36ee504974f2fe41b66be6640d78f36f23
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-13 14:36:53 +0300

    SocketAddr

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::net::TcpListener;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
 
 fn main() {
     let ipv4 = "127.0.0.1";
     let port = 8080;
     let listener = TcpListener::bind(format!("{ipv4}:{port}")).unwrap();
+    // let address = SocketAddr::from(([127, 0, 0, 1], port));
+    // let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+    // let listener = TcpListener::bind(address).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
