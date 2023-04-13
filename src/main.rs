4133f74eb28c3536b90cf5086aa7b42e651f455d
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-13 14:32:41 +0300

    bind

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello Rust book 20!");
+    let ipv4 = "127.0.0.1";
+    let port = 8080;
+    let listener = TcpListener::bind(format!("{ipv4}:{port}")).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
